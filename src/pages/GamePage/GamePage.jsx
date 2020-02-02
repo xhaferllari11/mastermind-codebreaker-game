ac92541f268339fc8f71bfb075e0c59614322a11
Alban <alban.xhaferllari@gmail.com>
2020-02-02 15:12:20 -0600

    added directions

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -42,6 +42,15 @@ const GamePage = (props) => {
       <footer className='App-header-footer'>
         {(props.winTries ? `You Won in ${props.winTries} Guesses!` : 'Good Luck!')}
       </footer>
+      <section style={{textAlign: 'center'}}>
+        <h3>How to play</h3>
+        <hr/>
+        <p>Goal: Guess the hidden combination.</p>
+        <p>The score tells you how close you got</p>
+        <p>Full circle: one of your guess is correct color and correct location.</p>
+        <p>Empty circle: you guessed a color correct, but in the wrong location.</p>
+        <p>Full rules about the game: <a href="https://en.wikipedia.org/wiki/Mastermind_(board_game)">Here</a></p>
+      </section>
     </div>
   );
 };
